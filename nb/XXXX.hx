6fd50b1a3bcf96e08c4270cd2e228927366e1967
0b1kn00b <polysemantic@gmail.com>
2022-04-07 13:03:59 +0200

    one page summary

---
+++ nb/XXXX.hx
@@ -0,0 +1,27 @@
+enum GComplexTypeSum{                                               enum GTypeDefKindSum {
+	GTPath(p:GTypePath);                                                GTDEnum;
+	GTFunction(args:Cluster<GComplexType>,ret:GComplexType);            GTDStructure;
+	GTAnonymous(fields:Cluster<GField>);                                GTDClass(?superClass:GTypePath,?interfaces:Cluster<GTypePath>,?isInterface,?isFinal:Bool,?isAbstract:Bool);
+	GTParent(t:GComplexType);                                           GTDAlias(t:GComplexType); // ignore TypeDefinition.fields
+	GTExtend(p:Cluster<GTypePath>,fields:Cluster<GField>);              GTDAbstract(tthis:Null<GComplexType>,?from:Cluster<GComplexType>,?to: Cluster<GComplexType>);
+	GTOptional(t:GComplexType);                                         GTDField(kind:GFieldType,?access:Cluster<GAccess>);
+	GTNamed(n:String,t:GComplexType);                                 }
+	GTIntersection(tl:Cluster<GComplexType>);                                               
+}
+enum GTypeDefKindSum {
+	GTDEnum;
+	GTDStructure;
+	GTDClass(?superClass:GTypePath,?interfaces:Cluster<GTypePath>,?isInterface:Bool,?isFinal:Bool,?isAbstract:Bool);
+	GTDAlias(t:GComplexType); // ignore TypeDefinition.fields
+	GTDAbstract(tthis:Null<GComplexType>,?from:Cluster<GComplexType>,?to:Cluster<GComplexType>);
+  GTDField(kind:GFieldType,?access:Cluster<GAccess>);
+}
+enum GFieldTypeSum {                                                                              GFunctionArgDef = name ?type:GCT,?opt ?value:GExpr ?meta
+	GFVar(t:Null<GComplexType>,?e:Null<GExpr>);                                                     GTypePathDef = name pack ?sub ?params
+	GFFun(f:GFunction);                                                                             GFieldDef = name kind:GFieldType ?access:Cluster<GAccess> ?meta ?doc
+	GFProp(get:GPropAccess,set:GPropAccess,?t:Null<GComplexType>,?e:Null<GExpr>);       
+}
+GTypeDefinitionDef = name pack kind:GTypeDefKind fields:Cluster<GField> ?params:Cluster<GTypeParamDecl> ?meta ?isExtern ?doc
+GTypeParamDeclDef = name ?constraints:Cluster<GComplexType> ?params:Cluster<GTypeParamDecl> ?meta ?defaultType:Null<GComplexType>;
+GFieldKind { Normal; Safe; }
+GAccessSum =  GAPublic	GAPrivate	GAStatic	GAOverride	GADynamic	GAInline	GAMacro	GAFinal	GAExtern	GAAbstract	GAOverload
